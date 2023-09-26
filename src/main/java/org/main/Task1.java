adb191ce2b14bd6b3e8b841df77bc01c96ff3633
Anton <anton.lop4t1n.98@gmail.com>
2023-09-27 01:37:24 +0300

    Made method for filling matrix before coding.

--- src/main/java/org/main/Task1.java
+++ src/main/java/org/main/Task1.java
@@ -1,59 +1,47 @@
 package org.main;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Task1 {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         System.out.println("Enter message to be encrypted: ");
-        while (scanner.hasNextLine()) {
-            String line = scanner.nextLine();
-            if (line.equals("EOF")) {
-                break;
-            }
+        String line = scanner.nextLine();
+        String[] parts = line.split(" ");
+        int key = parts[0].length();
+        String text = parts[1];
 
-            String[] parts = line.split(" ");
-            int key = Integer.parseInt(parts[0]);
-            String text = parts[1];
 
-            String encryptedText = encrypt(text, key);
-            System.out.println(encryptedText);
-        }
+        System.out.println("Matrix: ");
+        fillMatrix(key, text);
 
         scanner.close();
     }
 
-    public static String encrypt(String text, int key) {
-        int numRows = (int) Math.ceil((double) text.length() / key);
-        char[][] grid = new char[numRows][key];
-
+    private static String[][] fillMatrix(int key, String input) {
+        int numRows = (int) Math.ceil((double) input.length() / key);
+        String[][] matrix = new String[numRows][key];
         int index = 0;
-        int textLength = text.length();
-
-        for (int col = 0; col < key; col++) {
-            for (int row = 0; row < numRows; row++) {
-                if (index < textLength) {
-                    grid[row][col] = text.charAt(index);
-                    index++;
-                } else {
-                    grid[row][col] = ' ';
-                }
-            }
-        }
-
-        StringBuilder result = new StringBuilder();
+        boolean isD = true;
 
         for (int row = 0; row < numRows; row++) {
             for (int col = 0; col < key; col++) {
-                char currentChar = grid[row][col];
-                if (currentChar != ' ') {
-                    result.append(currentChar);
+                if (index < input.length()) {
+                    if (isD) {
+                        matrix[row][col] = input.substring(index, Math.min(index + 2, input.length()));
+                        index += 2;
+                    } else {
+                        matrix[row][col] = input.substring(index, index + 1);
+                        index += 1;
+                    }
+                    isD = !isD;
+                } else {
+                    matrix[row][col] = "EOF";
                 }
             }
         }
-
-        return result.toString();
+        return matrix;
     }
 }
