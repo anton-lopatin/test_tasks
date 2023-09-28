dbd00c9aa7cd649af35ae49d055972e6e0c01eb7
Anton <anton.lop4t1n.98@gmail.com>
2023-09-28 13:35:58 +0300

    Made method for assembling an encrypted string

--- src/main/java/org/main/Task1.java
+++ src/main/java/org/main/Task1.java
@@ -1,6 +1,8 @@
 package org.main;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 public class Task1 {
@@ -8,40 +10,77 @@ public class Task1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter message to be encrypted: ");
-        String line = scanner.nextLine();
-        String[] parts = line.split(" ");
-        int key = parts[0].length();
-        String text = parts[1];
+        while (scanner.hasNext()) {
+            String line = scanner.nextLine();
+            String[] parts = line.split(" ");
+            int columns = parts[0].length();
+            String key = parts[0];
+            String text = parts[1];
 
 
-        System.out.println("Matrix: ");
-        fillMatrix(key, text);
-
+            String encryptedMessage = encode(key, fillMatrix(columns, text));
+            System.out.println("Matrix: " + encryptedMessage);
+        }
         scanner.close();
     }
 
-    private static String[][] fillMatrix(int key, String input) {
-        int numRows = (int) Math.ceil((double) input.length() / key);
-        String[][] matrix = new String[numRows][key];
+    private static String[][] fillMatrix(int numColumns, String input) {
+        int numRows = input.length() / numColumns;
+        String[][] matrix = new String[numRows][numColumns];
         int index = 0;
-        boolean isD = true;
+        boolean isDigraph = true;
 
         for (int row = 0; row < numRows; row++) {
-            for (int col = 0; col < key; col++) {
+            for (int col = 0; col < numColumns; col++) {
                 if (index < input.length()) {
-                    if (isD) {
+                    if (isDigraph) {
                         matrix[row][col] = input.substring(index, Math.min(index + 2, input.length()));
                         index += 2;
                     } else {
                         matrix[row][col] = input.substring(index, index + 1);
                         index += 1;
                     }
-                    isD = !isD;
+                    isDigraph = !isDigraph;
                 } else {
-                    matrix[row][col] = "EOF";
+                    matrix[row][col] = "";
                 }
             }
         }
+
+        printMatrix(numRows, numColumns, matrix);
+
         return matrix;
+    }
+
+    private static String encode(String keyString, String[][] matrix) {
+        List<Integer> keyList = new ArrayList<>();
+        StringBuilder encryptedText = new StringBuilder();
+        for (int index = 0; index < keyString.split("").length; index++) {
+            keyList.add(Integer.parseInt(keyString.split("")[index]));
+        }
+
+        for (int key = 0; key < keyList.size(); key++) {
+            Integer minValue = Collections.min(keyList);
+            int minIndex = keyList.indexOf(minValue);
+            for (String[] strings : matrix) {
+                if (!strings[minIndex].equals("")) {
+                    encryptedText.append(strings[minIndex]);
+                }
+            }
+            keyList.set(minIndex, Integer.MAX_VALUE);
+        }
+        return encryptedText.toString();
+    }
+
+    private static void printMatrix(int numRows, int numColumns, String[][] matrix) {
+        for (int row = 0; row < numRows; row++) {
+            for (int col = 0; col < numColumns; col++) {
+                System.out.print((row + 1) + "." + (col + 1) + ")" + matrix[row][col]);
+                if (col < numColumns - 1) {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
