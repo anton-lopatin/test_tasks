608bf9f81bb5c14eb195a2f5d270376855294024
Anton <anton.lop4t1n.98@gmail.com>
2023-09-29 14:11:07 +0300

    Task 2 was completed

--- src/main/java/org/main/Task2.java
+++ src/main/java/org/main/Task2.java
@@ -18,7 +18,7 @@ public class Task2 {
                 sequence[i] = Integer.parseInt(numbers[i]);
             }
 
-            int[] nextElements = calculateNextElements(sequence, 3);
+            int[] nextElements = findNextTreeElems(sequence);
 
             System.out.println("Next three elements of sequence:");
             for (int i = 0; i < 3; i++) {
@@ -31,31 +31,63 @@ public class Task2 {
         scanner.close();
     }
 
-    public static int[] calculateNextElements(int[] sequence, int count) {
-        int n = sequence.length;
-        int[] result = new int[count];
+    private static int[] calculateDifference(int[] sequence) {
+        int length = sequence.length;
 
-        if (n >= 3) {
-            int d1 = sequence[1] - sequence[0];
-            int d2 = sequence[2] - sequence[1];
+        if (length <= 1) {
+            return sequence;
+        }
 
-            int nextElement = sequence[n - 1] + d2;
+        for (int index = 0; index < length - 1; index++) {
+            if (sequence[index + 1] - sequence[index] == 0) {
+                return sequence;
+            }
+        }
 
-            result[count - 3] = nextElement;
-            result[count - 2] = nextElement + d1;
-            result[count - 1] = nextElement + d1 + d2;
-        } else if (n == 2) {
+        int[] subArray = new int[length - 1];
+        for (int index = 0; index < subArray.length; index++) {
+            subArray[index] = sequence[index + 1] - sequence[index];
+        }
+
+        return calculateDifference(subArray);
+    }
+
+    private static int[] findNextTreeElems(int[] sequence) {
+        double triangleNumber = (Math.sqrt(8 * sequence[0] + 1) - 1) / 2;
+        if (triangleNumber == (int) triangleNumber && (int) triangleNumber != 1) {
+            int[] differences = calculateDifference(sequence);
+            int dif = sequence.length * differences[0] + 1;
+            int nextElem1 = sequence[sequence.length - 1] + dif + differences[0];
+            dif += differences[0];
+            int nextElem2 = nextElem1 + dif + differences[1];
+            dif += differences[0];
+            int nextElem3 = nextElem2 + dif + differences[2];
+
+            return new int[]{nextElem1, nextElem2, nextElem3};
+        }
+
+        int length = sequence.length;
+        int[] result = new int[3];
+
+        if (length >= 3) {
+            int difference1 = sequence[1] - sequence[0];
+            int difference2 = sequence[2] - sequence[1];
+
+            int nextElement = sequence[length - 1] + difference2;
+
+            result[0] = nextElement;
+            result[1] = nextElement + difference1;
+            result[2] = nextElement + difference1 + difference2;
+        } else if (length == 2) {
             int d1 = sequence[1] - sequence[0];
             int nextElement = sequence[1] + d1;
 
-            result[count - 2] = nextElement;
-            result[count - 1] = nextElement + d1;
-        } else if (n == 1) {
+            result[1] = nextElement;
+            result[0] = nextElement + d1;
+        } else {
             int nextElement = sequence[0] + 1;
-
-            result[count - 1] = nextElement;
+            result[0] = nextElement;
         }
-
         return result;
     }
 }
